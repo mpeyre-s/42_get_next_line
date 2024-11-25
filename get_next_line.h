d448c6249b924b88a0d0db5788aa55cb0d9da753
mpeyre-s <mathispeyre0@gmail.com>
2024-11-25 10:28:54 +0100

    init files

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mathispeyre <mathispeyre@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 10:11:59 by mathispeyre       #+#    #+#             */
+/*   Updated: 2024/11/25 10:25:54 by mathispeyre      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
